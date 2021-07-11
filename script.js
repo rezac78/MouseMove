4b1cca585a4c1224937a12846abb708e766660ae
reza <69678342+rezac78@users.noreply.github.com>
2021-07-11 09:05:14 -0700

    mouse

---
+++ script.js
@@ -0,0 +1,30 @@
+// DOM => Document Object Model
+
+document.onmousemove = animateCircle;
+
+function animateCircle(event) {
+  var circle = document.createElement('div')
+  // console.log(circle)
+  var colors = ["#f00", "#6cf", "#0f0", "#00f"]
+  circle.setAttribute('class', 'circle')
+
+  // console.log(event)
+
+  circle.style.left = event.clientX + "px"
+  circle.style.top = event.clientY + "px"
+  circle.style.transition = "all 0.5s linear"
+
+  document.body.appendChild(circle)
+
+  circle.style.left = circle.offsetLeft - 20 + "px"
+  circle.style.top = circle.offsetTop - 20 + "px"
+
+  var mainColor = colors[Math.floor(Math.random() * colors.length)]
+
+  circle.style.borderColor = mainColor
+  circle.style.width = "50px"
+  circle.style.height = "50px"
+  circle.style.borderWidth = "5px"
+  circle.style.opacity = 0
+
+}
